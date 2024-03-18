f072f53d34b6553f55eca89f8b13503097a7c71b
Muhammad Dedishov <moxmad99@mail.ru>
2024-03-18 21:08:58 +0300

    push to repo

---
+++ js/script.js
@@ -0,0 +1,203 @@
+const navbar = document.querySelector('.navbar')
+const logoLight = document.querySelector('.logo-light')
+const logo = document.querySelector('.logo')
+const toggleMenu = document.querySelector('.mobile-menu-toggle')
+const menu = document.querySelector('.mobile-menu')
+const modal = document.querySelector('.modal')
+const isFront = document.body.classList.contains('front-page')
+
+const lightModeOn = () => {
+    navbar.classList.add('navbar-light')
+    logo.style.display = 'flex'
+    logoLight.style.display = 'none'
+}
+
+const lightModeOff = () => {
+    navbar.classList.remove('navbar-light')
+    logo.style.display = 'none'
+    logoLight.style.display = 'flex'
+}
+
+const changeNavHeight = (height) => {
+    navbar.style.height = height;
+}
+
+const openMenu = (e) => {
+    menu.classList.add('is-open');
+    toggleMenu.classList.add('close-menu')
+    document.body.style.overflow = 'hidden';
+    lightModeOn()
+}
+
+const closeMenu = (e) => {
+    menu.classList.remove('is-open');
+    toggleMenu.classList.remove('close-menu')
+    document.body.style.overflow = '';
+    if (isFront) {
+        window.scrollY > 1 ? lightModeOn() : lightModeOff()
+    }
+}
+
+const closeModal = () => {
+    modal.classList.remove('is-open')
+}
+
+
+window.addEventListener('scroll', () => {
+    this.scrollY > 1 ? changeNavHeight('4.5rem') : changeNavHeight('5.875rem')
+    if (isFront) {
+        this.scrollY > 1 ? lightModeOn() : lightModeOff()
+    }
+})
+
+document.addEventListener('keyup', (e) => {
+    e.preventDefault()
+    if (e.code == 'Escape' && modal.classList.contains('is-open')) {
+        closeModal()
+    }
+})
+
+document.addEventListener('click', (e) => {
+    if (e.target.closest('[data-toggle=modal]')) {
+        modal.classList.add('is-open')
+    } if (modal.classList.contains('is-open')) {
+        if (e.target.classList.contains('modal') || e.target.closest('.modal-close')) {
+            closeModal()
+        }
+    } if (e.target.closest('.mobile-menu-toggle')) {
+        e.preventDefault()
+        !menu.classList.contains('is-open') ? openMenu() : closeMenu()
+    }
+})
+
+const swiper = new Swiper('.swiper-first', {
+    slidesPerView: 1,
+    speed: 400,
+    autoHeight: true,
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3
+        },
+        1024: {
+            slidesPerView: 4
+        },
+        1200: {
+            slidesPerView: 5
+        }
+    }
+});
+
+const secondSwiper = new Swiper('.swiper-second', {
+    slidesPerView: 1,
+    speed: 400,
+    autoHeight: true,
+    // centeredSlides: true,
+    slidesOffsetBefore: 0,
+    navigation: {
+        nextEl: '.step-next',
+        prevEl: '.step-prev',
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3
+        },
+        1024: {
+            slidesPerView: 4
+        },
+    }
+});
+
+const blogSwiper = new Swiper('.blog-slider', {
+    slidesPerView: 1,
+    speed: 400,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.blog-next',
+        prevEl: '.blog-prev',
+    },
+    breakpoints: {
+        995: {
+            slidesPerView: 2,
+        },
+    }
+});
+
+/* Создаем префикс +7, даже если вводят 8 или 9 */
+const prefixNumber = (str) => {
+    /* если вводят семерку, добавляем ей скобку */
+    if (str === "7") {
+        return "7 (";
+    }
+    /* если вводят восьмерку, ставим вместо нее +7 ( */
+    if (str === "8") {
+        return "+7 (";
+    }
+    /* если пишут девятку, заменяем на +7 (9  */
+    if (str === "9") {
+        return "7 (9";
+    }
+    /* в других случаях просто 7 (  */
+    return "7 (";
+}; /* профикс в любом раскладе будет +7 () */
+
+/* Ловим события ввода в любом поле */
+document.addEventListener("input", (e) => {
+    /* Проверяем, что это поле имеет класс phone-mask */
+    if (e.target.classList.contains("phone-mask")) {
+        /* поле с телефоном помещаем в переменную input */
+        const input = e.target;
+        /* вставляем плюс в начале номера */
+        const value = input.value.replace(/\D+/g, "");
+        /* длинна номера 11 символов */
+        const numberLength = 11;
+
+        /* Создаем переменную, куда будем записывать номер */
+        let result;
+        /* Если пользователь ввел 8... */
+        if (input.value.includes("+8") || input.value[0] === "8") {
+            /* Стираем восьмерку */
+            result = "";
+        } else {
+            /* Оставляем плюсик в поле */
+            result = "+";
+        }
+
+        /* Запускаем цикл, где переберем каждую цифру от 0 до 11 */
+        for (let i = 0; i < value.length && i < numberLength; i++) {
+            switch (i) {
+                case 0:
+                    /* в самом начале ставим префикс +7 ( */
+                    result += prefixNumber(value[i]);
+                    continue;
+                case 4:
+                    /* добавляем после "+7 (" круглую скобку ")" */
+                    result += ") ";
+                    break;
+                case 7:
+                    /* дефис после 7 символа */
+                    result += "-";
+                    break;
+                case 9:
+                    /* еще дефис  */
+                    result += "-";
+                    break;
+                default:
+                    break;
+            }
+            /* на каждом шаге цикла добавляем новую цифру к номеру */
+            result += value[i];
+        }
+        /* итог: номер в формате +7 (999) 123-45-67 */
+        input.value = result;
+    }
+});
